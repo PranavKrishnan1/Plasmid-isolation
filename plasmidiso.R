b2fcfc8e4ac9ccbaa9af9026f855a0f2b5135da9
Pranav Krishnan <Pranav.krishnan.kp@gmail.com>
2018-09-01 11:28:39 +0530

    second commit

--- plasmidiso.R
+++ plasmidiso.R
@@ -1,6 +1,13 @@
 
-daata = read.table("plasmidisodaata3.txt", header = TRUE)
-Plot(daata,xaxt="n",yaxt="n",col=c("black"),pch=c(1),main="Colonies mapped against antibiotic resistance")
+daata1 = read.table("plasmidisodaata1.txt", header = TRUE)
+daata3 = read.table("plasmidisodaata3.txt", header = TRUE)
+par( mfrow = c( 2, 2 ) )
+plot(daata1,sub="trial 1",xaxt="n",yaxt="n",col=c("red"),pch=c(21),main="Colonies mapped against antibiotic resistance")
 axis(2,at=0:4,labels=c("nan","kan","cmp","cip","tet"))
 axis(1,at=1:46,labels=c(1:46))
+plot(daata3,sub = "trial 3",xaxt="n",yaxt="n",col=c("blue"),pch=c(22),main="Colonies mapped against antibiotic resistance")
+axis(2,at=0:5,labels=c("nan","nan","kan","cmp","cip","tet"))
+axis(1,at=1:46,labels=c(1:46))
+
+
 

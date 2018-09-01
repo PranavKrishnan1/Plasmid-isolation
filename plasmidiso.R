93ea9c508e7693c9380ab1008743aac1a2239363
Pranav Krishnan <Pranav.krishnan.kp@gmail.com>
2018-09-01 10:30:29 +0530

    update

--- plasmidiso.R
+++ plasmidiso.R
@@ -1,6 +1,6 @@
 
 daata = read.table("plasmidisodaata3.txt", header = TRUE)
-plot(daata,xaxt="n",yaxt="n",col=c("black"),pch=c(1))
-axis(2,at=1:4,labels=c("kan","cmp","cip","tet"))
-axis(1,at=1:30,labels=c(1:30))
+Plot(daata,xaxt="n",yaxt="n",col=c("black"),pch=c(1),main="Colonies mapped against antibiotic resistance")
+axis(2,at=0:4,labels=c("nan","kan","cmp","cip","tet"))
+axis(1,at=1:46,labels=c(1:46))
 

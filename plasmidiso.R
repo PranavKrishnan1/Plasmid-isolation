828c76039cce5fd78f17d22e268557131a8b9662
Pranav Krishnan <Pranav.krishnan.kp@gmail.com>
2018-09-03 14:51:05 +0530

    third commit

--- plasmidiso.R
+++ plasmidiso.R
@@ -1,13 +1,21 @@
 
 daata1 = read.table("plasmidisodaata1.txt", header = TRUE)
+daata2 = read.table("plasmidisodaata2.txt", header = TRUE)
 daata3 = read.table("plasmidisodaata3.txt", header = TRUE)
 par( mfrow = c( 2, 2 ) )
-plot(daata1,sub="trial 1",xaxt="n",yaxt="n",col=c("red"),pch=c(21),main="Colonies mapped against antibiotic resistance")
-axis(2,at=0:4,labels=c("nan","kan","cmp","cip","tet"))
+plot(daata1,sub="trial 1",xaxt="n",yaxt="n",col=c("red"),pch=c(1),main="Colonies mapped against antibiotic resistance")
+axis(2,at=0:4,labels=c("nan","kan","cam","cip","tet"))
 axis(1,at=1:46,labels=c(1:46))
-plot(daata3,sub = "trial 3",xaxt="n",yaxt="n",col=c("blue"),pch=c(22),main="Colonies mapped against antibiotic resistance")
-axis(2,at=0:5,labels=c("nan","nan","kan","cmp","cip","tet"))
+plot(daata2,sub="trial 2",xaxt="n",yaxt="n",col=c("green"),pch=c(3),main="Colonies mapped against antibiotic resistance")
+axis(2,at=0:4,labels=c("nan","kan","cam","cip","tet"))
 axis(1,at=1:46,labels=c(1:46))
-
+plot(daata3,sub = "trial 3",xaxt="n",yaxt="n",col=c("blue"),pch=c(4),main="Colonies mapped against antibiotic resistance")
+axis(2,at=0:5,labels=c("nan","nan","kan","cam","cip","tet"))
+axis(1,at=1:46,labels=c(1:46))
+plot(daata1,col="red",pch=1,xaxt="n",yaxt="n")
+axis(2,at=0:4,labels=c("nan","kan","cam","cip","tet"))
+axis(1,at=1:46,labels=c(1:46))
+points(daata2,col="green",pch=3)
+points(daata3,col="blue",pch=4)
 
 
